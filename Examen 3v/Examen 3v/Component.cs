785fd121a7759d66dd7c87a1210b324aeeb29b62
Sarah <scartleha1123@gmail.com>
2025-05-08 15:21:36 +0200

    Apartado 1
    Created Start and Update and added them as the form they should be

---
+++ Examen 3v/Examen 3v/Component.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_3v
+{
+    internal class Component : Game_Object
+    {
+        public virtual void Start() { }
+        public virtual void Update() { }
+    }
+}
