a08ec4e16fd5479a58a1578eab73c8bb5fff12ee
Sarah <scartleha1123@gmail.com>
2025-05-08 16:07:31 +0200

    Apartado 3
    Creado el GameObject

---
+++ Examen 3v/Examen 3v/Game_Object.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_3v
+{
+    internal class Game_Object 
+    {
+        private List<Component> components;
+        public Game_Object()
+        {
+            components = new List<Component>();
+        }
+        public void AddComponent(Component comp)
+        {
+            components.Add(comp);
+        }
+        public void Start()
+        {
+            foreach (Component comp in components) { comp.Start(); }
+        }
+        public void Update()
+        {
+            foreach (Component comp in components) { comp.Update(); }
+        }
+    }
+}
